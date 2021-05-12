d12f906063f06d07e4ce47c848c4f24be0776139
amphyzale <enforcer.snk@gmail.com>
2021-05-12 14:35:00 +0300

    fix cors

--- src/main/java/net/courseproject/alex/veterinary/config/WebSecurityConfig.java
+++ src/main/java/net/courseproject/alex/veterinary/config/WebSecurityConfig.java
@@ -38,6 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .csrf()
+                .and()
+                .cors()
                 .disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
