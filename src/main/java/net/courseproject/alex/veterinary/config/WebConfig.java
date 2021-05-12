249d79c09beb5be942ad3bdeae398ff2f7f67b9b
amphyzale <enforcer.snk@gmail.com>
2021-05-12 23:30:12 +0300

    fix cors

--- src/main/java/net/courseproject/alex/veterinary/config/WebConfig.java
+++ src/main/java/net/courseproject/alex/veterinary/config/WebConfig.java
@@ -2,7 +2,6 @@ package net.courseproject.alex.veterinary.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-                .allowedOrigins("*", "https://veterinary-front.herokuapp.com", "http://localhost:4200", "https://localhost:4200")
+                .allowedOrigins("https://veterinary-front.herokuapp.com", "http://localhost:4200", "https://localhost:4200")
                 .allowCredentials(true);
     }
 }
