4b76c01a653ffb0f5d9d850bac3cca4091b771c8
amphyzale <enforcer.snk@gmail.com>
2021-05-23 21:02:51 +0300

    add appointments

--- src/main/java/net/courseproject/alex/veterinary/dto/request/AppointmentRequest.java
+++ src/main/java/net/courseproject/alex/veterinary/dto/request/AppointmentRequest.java
@@ -1,6 +1,7 @@
 package net.courseproject.alex.veterinary.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -9,8 +10,12 @@ import java.time.LocalDateTime;
 @Data
 @Accessors(chain = true)
 public class AppointmentRequest {
+    @JsonProperty("start_date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime start;
+    @JsonProperty("end_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime end;
     private Long serviceId;
     private Long userId;
     private Long doctorId;
--- src/main/java/net/courseproject/alex/veterinary/dto/response/AppointmentResponse.java
+++ src/main/java/net/courseproject/alex/veterinary/dto/response/AppointmentResponse.java
@@ -1,6 +1,7 @@
 package net.courseproject.alex.veterinary.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import net.courseproject.alex.veterinary.domain.Status;
@@ -11,8 +12,10 @@ import java.time.LocalDateTime;
 @Accessors(chain = true)
 public class AppointmentResponse {
     private Long id;
+    @JsonProperty("start_date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime start;
+    @JsonProperty("end_date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime end;
     private ServiceResponse service;
